ed5ed3d5b8babdd6ad0d592a70a4bd0c94172a2d
srinu255 <thrishankgithub@gmail.com>
2024-12-10 13:35:26 +0530

    fst cmmited

---
+++ src/main/java/com/org/java/SpringbootTomcatJenkinPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTomcatJenkinPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTomcatJenkinPipelineApplication.class, args);
+	}
+
+}
